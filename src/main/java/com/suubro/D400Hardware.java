dd290eb946cfe40abd545a9dbc66e90daa7c7df1
SuuBro <J.Subra@gmail.com>
2022-06-26 11:40:46 +0100

    More fields

--- src/main/java/com/suubro/D400Hardware.java
+++ src/main/java/com/suubro/D400Hardware.java
@@ -9,7 +9,7 @@ import java.util.Arrays;
 public class D400Hardware
 {
     public static final int JOG_WHEEL       = 118;
-    public static final int SHUTTLE         = 121;
+    public static final int SHUTTLE         = 119;
 
     public static final int BTN_UP          = 46;
     public static final int BTN_DOWN        = 47;
@@ -24,6 +24,48 @@ public class D400Hardware
     public static final int BTN_REC         = 95;
     public static final int BTN_ASSIGN1     = 116;
     public static final int BTN_ASSIGN2     = 117;
+
+    public static final int TOUCH_FADER_1   = 104;
+    public static final int TOUCH_FADER_2   = 105;
+    public static final int TOUCH_FADER_3   = 106;
+    public static final int TOUCH_FADER_4   = 107;
+    public static final int TOUCH_FADER_5   = 108;
+    public static final int TOUCH_FADER_6   = 109;
+    public static final int TOUCH_FADER_7   = 110;
+    public static final int TOUCH_FADER_8   = 111;
+
+    public static final int BTN_RECORD_1    = 0;
+    public static final int BTN_RECORD_2    = 1;
+    public static final int BTN_RECORD_3    = 2;
+    public static final int BTN_RECORD_4    = 3;
+    public static final int BTN_RECORD_5    = 4;
+    public static final int BTN_RECORD_6    = 5;
+    public static final int BTN_RECORD_7    = 6;
+    public static final int BTN_RECORD_8    = 7;
+    public static final int BTN_SOLO_1      = 8;
+    public static final int BTN_SOLO_2      = 9;
+    public static final int BTN_SOLO_3      = 10;
+    public static final int BTN_SOLO_4      = 11;
+    public static final int BTN_SOLO_5      = 12;
+    public static final int BTN_SOLO_6      = 13;
+    public static final int BTN_SOLO_7      = 14;
+    public static final int BTN_SOLO_8      = 15;
+    public static final int BTN_MUTE_1      = 16;
+    public static final int BTN_MUTE_2      = 17;
+    public static final int BTN_MUTE_3      = 18;
+    public static final int BTN_MUTE_4      = 19;
+    public static final int BTN_MUTE_5      = 20;
+    public static final int BTN_MUTE_6      = 21;
+    public static final int BTN_MUTE_7      = 22;
+    public static final int BTN_MUTE_8      = 23;
+    public static final int BTN_SELECT_1    = 24;
+    public static final int BTN_SELECT_2    = 25;
+    public static final int BTN_SELECT_3    = 26;
+    public static final int BTN_SELECT_4    = 27;
+    public static final int BTN_SELECT_5    = 28;
+    public static final int BTN_SELECT_6    = 29;
+    public static final int BTN_SELECT_7    = 30;
+    public static final int BTN_SELECT_8    = 31;
 
     public static final int EQ_KNOB_BTN_1   = 64;
     public static final int EQ_KNOB_1       = 96;
@@ -54,7 +96,6 @@ public class D400Hardware
 
     private final MidiOut         portOut;
     private final int []          ledCache             = new int [128];
-
 
     public D400Hardware(final MidiOut outputPort, final MidiIn inputPort, final ShortMidiDataReceivedCallback inputCallback)
     {
